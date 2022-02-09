718154445bc6d7003c265c6d3704165bb338fb69
pen lum <swk5373@gmail.com>
2022-02-09 18:32:56 +0900

    Fix: ClickToEdit.js의 input box 클릭 시 디자인 수정

--- src/component/ClickToEdit.js
+++ src/component/ClickToEdit.js
@@ -1,44 +1,28 @@
-import React, {useState, useEffect, useRef} from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components';
 import {Basic, Container} from './Style';
 
 const ClickToEdit=()=>{
 
-    const [name, setName] = useState('dd');
-    const [age, setAge] = useState('dd');
+    const [name, setName] = useState('');
+    const [age, setAge] = useState('');
     const [prevName, setPrevName] = useState('');
     const [prevAge, setPrevAge] = useState('');
     
-    let prevInput = useRef();
-    let nowInput = useRef();
-
-    useEffect(()=>{
-        window.addEventListener("click", ()=>{
-            if(prevInput !== nowInput){
-
-                setPrevAge(age);
-                setPrevName(name);
-        
-            }
-        });
-    })
-
-    const selectedInput = (e) =>{
-
-        prevInput = e.target;
-    }
-    
-  
 
     return(
         <>
             <Container>
                 <h2>Click To Edit</h2>
                 <CTEBasic>
-                   이름 <InputBox value={name} onClick={selectedInput} onChange={(e) => {setName(e.target.value)}} /> 
+                   이름 <InputBox value={name} onBlur={()=>{
+                       setPrevName(name);
+                   }} onChange={(e) => {setName(e.target.value)}} /> 
                 </CTEBasic>
                 <CTEBasic>
-                   나이 <InputBox value={age} onClick={selectedInput} onChange={(e) => {setAge(e.target.value)}}/>
+                   나이 <InputBox value={age} onBlur={()=>{
+                       setPrevAge(age);
+                   }} onChange={(e) => {setAge(e.target.value)}}/>
                 </CTEBasic>
                 <CTEBasic>
                     이름 {prevName} 나이 {prevAge}
@@ -55,6 +39,13 @@ const CTEBasic = styled(Basic)`
 const InputBox = styled.input`
     width: 150px;
     height: 30px;
+    &:focus{
+        outline:3px solid skyblue;
+        border:none;
+        width: 153px;
+        height: 33px;
+
+    }
 `
 
 
